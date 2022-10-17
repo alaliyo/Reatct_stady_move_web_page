c6425e74801944bd4b34014726f7e51c90919c8f
alaliyo <des321321@naver.com>
2022-10-17 10:04:16 +0900

    feat : Deps

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,15 +1,40 @@
 import { useState, useEffect } from "react";
 
 function App() {
+  const [keyword, setkeyword] = useState("");
   const [conter, setValue] = useState(0);
+  
+  const onChange = (event) => setkeyword(event.target.value);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("api");
-  const RunOnOne = () => {
-    console.log("1번");
-  }
-  useEffect(RunOnOne, [])
+
+  useEffect(() => {
+    console.log("api");
+  }, []);
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("keyword");
+    }
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("conter");
+  }, [conter])
+
+  useEffect(() => {
+    console.log("메롱")
+  }, [conter, keyword])
+
+
+  
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="검색창"
+      />
       <h1>{conter}</h1>
       <button onClick={onClick}>클릭</button>
     </div>
